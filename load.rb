3b87f54ed8f0e8245265eeb359be04673384401a
Hiroyuki Sato <hiroysato@gmail.com>
2015-09-03 13:50:11 +0900

    Update load script

--- load.rb
+++ load.rb
@@ -8,11 +8,12 @@ Groonga::Database.open("database/data.db")
 data_table = Groonga["Data"]
 
 entry_num = 0
-
-CSV.foreach("data/data.txt").each do |row|
-  entry_num += 1
-  article = row[0]
-  line_no = row[1]
-  data_table.add(entry_num.to_s, {'line_no' => line_no,
-                             'article' => article })
+1.upto(30) do 
+  CSV.foreach("data/single.txt").each_with_index do |row,idx|
+    entry_num += 1
+    article = row[0]
+    line_no = idx + 1
+    data_table.add(entry_num.to_s, {'line_no' => line_no,
+                                    'article' => article })
+  end
 end
